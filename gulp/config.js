5af8db98495677e532a6085d818f7778391e0387
Shyam Guthikonda <shyamguth@gmail.com>
2015-03-15 09:16:55 -0400

    Add gulp tasks.

---
+++ gulp/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    browserSync: {
+        server: {
+            // Serve up our build folder
+            baseDir: './examples/dist'
+        }
+    },
+    browserify: {
+        // A separate bundle will be generated for each
+        // bundle config in the list below
+        bundleConfigs: [{
+            entries: './examples/01/index.js',
+            dest: './examples/dist/01',
+            outputName: 'index.js',
+            // list of externally available modules to exclude from the bundle
+            external: []
+        }]
+    }
+};
