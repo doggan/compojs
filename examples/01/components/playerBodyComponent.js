df1c7864bfe7b5421fe5cb4d7f8ea17ec87065d3
Shyam Guthikonda <shyamguth@gmail.com>
2015-03-16 00:03:22 -0400

    Add player animation.

--- examples/01/components/playerBodyComponent.js
+++ examples/01/components/playerBodyComponent.js
@@ -1,10 +1,14 @@
 /* global PIXI */
 'use strict';
 
-var MIN_X = 20;
+var MIN_X = 10;
 var MAX_X = 400 - MIN_X;
-var MIN_Y = 30;
-var MAX_Y = 290;
+var MIN_Y = 20;
+var MAX_Y = 295;
+
+function clamp(val, min, max) {
+    return Math.min(Math.max(val, min), max);
+}
 
 module.exports = function() {
     var graphics;
@@ -16,9 +20,9 @@ module.exports = function() {
 
         graphics = new PIXI.Graphics();
 
-        graphics.beginFill(0xEE6352);
-        graphics.lineStyle(2, 0x3FA7D6, 1);
-        graphics.drawPolygon([-10, 0, 10, 0, 0, -20]);
+        graphics.beginFill(0xF72C25);
+        graphics.lineStyle(1, 0x071013, 1);
+        graphics.drawPolygon([-6, 0, 6, 0, 0, -14]);
 
         graphics.position.x = windowSize.width * 0.5;
         graphics.position.y = MAX_Y;
@@ -26,9 +30,32 @@ module.exports = function() {
         rendererSystem.addToStage(graphics);
     };
 
-    function clamp(val, min, max) {
-        return Math.min(Math.max(val, min), max);
-    }
+    var fromScale = 1.0;
+    var toScale = 1.25;
+    var elapsedScaleTime = 0;
+    var totalScaleTime = 0.25;
+    var update = function() {
+        var dt = this.entity.engine.time.deltaTime;
+
+        // Scale animation.
+        var scale;
+        var t = elapsedScaleTime / totalScaleTime;
+        if (t >= 1.0) {
+            t = 1.0;
+            scale = toScale;
+            elapsedScaleTime = 0;
+
+            var tmp = fromScale;
+            fromScale = toScale;
+            toScale = tmp;
+        } else {
+            elapsedScaleTime += dt;
+            scale = ((1.0 - t) * fromScale) + (t * toScale);
+        }
+
+        graphics.scale.x = scale;
+        graphics.scale.y = scale;
+    };
 
     var move = function(x, y) {
         var newX = graphics.position.x + x;
@@ -43,6 +70,7 @@ module.exports = function() {
 
     return {
         start: start,
+        update: update,
 
         getPosition: function() {
             return {
