34880ddb1ab9da3b644995ed996615804fa7f0fa
Shyam Guthikonda <shyamguth@gmail.com>
2015-03-16 12:34:32 -0400

    Add example 0 to build process.

--- gulp/config.js
+++ gulp/config.js
@@ -9,10 +9,14 @@ module.exports = {
         // A separate bundle will be generated for each
         // bundle config in the list below
         bundleConfigs: [{
+            entries: './examples/00/index.js',
+            dest: './examples/dist/js/00',
+            outputName: 'index.js',
+            external: []
+        }, {
             entries: './examples/01/index.js',
             dest: './examples/dist/js/01',
             outputName: 'index.js',
-            // list of externally available modules to exclude from the bundle
             external: []
         }]
     }
